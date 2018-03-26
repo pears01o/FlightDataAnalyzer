ba8a359f04d0e16a7a39da6eab47d645ca0b5734
Richard Hoy <richard.hoy@flightdataservices.com>
2018-03-26 09:53:03 +0100

    AE-1729: Add ability to print the ordered tree

--- analysis_engine/dependency_graph.py
+++ analysis_engine/dependency_graph.py
@@ -2,9 +2,10 @@ from __future__ import print_function
 
 import os
 import sys
-import logging 
+import logging
 import networkx as nx # pip install networkx or /opt/epd/bin/easy_install networkx
 import six
+import copy
 
 from collections import deque
 
@@ -43,7 +44,7 @@ before Latitude Lat/Long or is it more important to
 calculate the Lat/Long from the Groundspeed? The priority should include the
 fact that if a parameter is recorded, it is already available.
 
-Q: 
+Q:
 
 * Nice to have: reverse digraph to get arrows poitning towards the root - use
   pre's rather than successors in tree traversal
@@ -66,8 +67,8 @@ class CircularDependency(KeyError):
 
 def print_ordered_tree(tree_path):
     '''
-    This is tool that prints the order and the intended tree in which nodes are traversed. 
-    It shows: 
+    This is tool that prints the order and the intended tree in which nodes are traversed.
+    It shows:
     -	The path depth that the node appears
     -	Where the node will be included into the process_order. It will show the number in the processing order that the node will be created, indicated by Node Name (order)
     -	If node's dependencies not satisfied the can_operate, indicated by [Node Name] (INOP)
@@ -90,30 +91,45 @@ def print_ordered_tree(tree_path):
     1:		- 'Turbulence During Cruise Max' (440) 	 'root' > 'Turbulence During Cruise Max'
     0:	- 'root' (713) 	 'root'
     '''
+    for line in format_ordered_tree(tree_path):
+        print(line[:-1])
+
+def format_ordered_tree(tree_path):
     order = 0
-    for path in tree_path[:]:
+    lines = []
+    tree = copy.deepcopy(tree_path)
+    for path in tree[:]:
         if path[-1] == 'NOT OPERATIONAL':
             path.pop()
-            print("%d:%s- ['%s'] (INOP) \t '%s'" % (len(path)-1, '\t'*len(path), path[-1], "' > '".join(path)))
+            lines.append("%d:%s- ['%s'] (INOP) \t '%s'\n" % (len(path)-1, '\t'*len(path), path[-1], "' > '".join(path)))
         elif path[-1] == 'CIRCULAR':
             path.pop()
-            print("%d:%s- <<< '%s' CIRCULAR >>> \t '%s' ><<< '%s' CIRCULAR >>>" % (len(path)-1, '\t'*len(path), path[-1], "' > '".join(path), path[-1]))
+            lines.append("%d:%s- <<< '%s' CIRCULAR >>> \t '%s' ><<< '%s' CIRCULAR >>>\n" % (len(path)-1, '\t'*len(path), path[-1], "' > '".join(path), path[-1]))
         else:
-            print("%d:%s- '%s' (%d) \t '%s'" % (len(path)-1, '\t'*len(path), path[-1], order, "' > '".join(path)))    
+            lines.append("%d:%s- '%s' (%d) \t '%s'\n" % (len(path)-1, '\t'*len(path), path[-1], order, "' > '".join(path)))
             order += 1
+    return lines
 
-def indent_tree(graph, node, level=0, space='  ', delim='- ', label=True, 
+def ordered_tree_to_file(tree_path, name='ordered_tree_path.txt'):
+    '''
+    Same as print_ordered_tree but to file.
+    '''
+    with open(name, 'w') as f:
+        f.writelines(format_ordered_tree(tree_path))
+
+
+def indent_tree(graph, node, level=0, space='  ', delim='- ', label=True,
                 recurse_active=True):
     '''
     Small tool to assist representing a tree on the console.
-    
+
       print('\n'.join(indent_tree(gr_all, 'root')))
-      
+
       - root
         - sub1
           - sub2
         - sub3
-      
+
     :param graph: Entire graph
     :type graph: nx.DiGraph
     :param node: Node to start recursing successors from
@@ -129,7 +145,7 @@ def indent_tree(graph, node, level=0, space='  ', delim='- ', label=True,
     :param recurse_active: Whether to show the tree for active params
     :type recurse_active: Boolean
     '''
-    
+
     def recurse_tree(node, level):
         if node in path:
             # circular dependency started!
@@ -153,7 +169,7 @@ def indent_tree(graph, node, level=0, space='  ', delim='- ', label=True,
             path.pop()
             level_rows.extend(sub_level)
         return level_rows
-    
+
     path = deque()  # current branch path
     return recurse_tree(node, level)
 
@@ -161,7 +177,7 @@ def indent_tree(graph, node, level=0, space='  ', delim='- ', label=True,
 def print_tree(graph, node='root', **kwargs):
     '''
     Helper to shortcut printing of indent_tree.
-    
+
     See indent_tree for help with args/kwargs.
     '''
     print('\n'.join(indent_tree(graph, node, **kwargs)))
@@ -171,32 +187,32 @@ def dependencies3(di_graph, root, node_mgr, raise_cir_dep=False):
     '''
     Performs a Depth First Search down each dependency node in the tree
     (di_graph) until each branch's dependencies are best satisfied.
-    
+
     Avoids circular dependencies within the DiGraph by building up a path of
     the nodes visited down the current depth search and if encountering a
     node already visited (in the path) this node is declared unavailable.
-    
+
     This allows nodes to depend upon a possible circular dependency which may
     or may not exist depending on the recorded parameters within the frame.
-    
+
     e.g.
     Heading -> Heading True + Magnetic Variation
     Heading True -> Heading - Magnetic Variation
-    
+
     :param di_graph: Directed graph of all nodes and their dependencies.
     :type di_graph: nx.DiGraph
     :param root: Root node to start traversing from, usually named 'root'
     :type root: String
-    :param node_mgr: Node manager which can assess whether nodes are 
-                     operational with the available dependencies at each 
+    :param node_mgr: Node manager which can assess whether nodes are
+                     operational with the available dependencies at each
                      layer of the tree.
     :type node_mgr: analysis_engine.node.NodeManager
     :raise_cir_dep: Stop and raise a CircularDependency error if a circular
-                    dependency on the node is encountered. 
+                    dependency on the node is encountered.
     '''
     log_stuff = logger.getEffectiveLevel() >= logging.INFO
     def traverse_tree(node):
-        "Begin the recursion at this node's position in the dependency tree"     
+        "Begin the recursion at this node's position in the dependency tree"
         if node in path:
             # add node for it to be removed (pop'd) in a moment
             path.append(node)
@@ -215,7 +231,7 @@ def dependencies3(di_graph, root, node_mgr, raise_cir_dep=False):
         if node in active_nodes:
             # node already discovered operational
             return True
-        
+
         layer = set()  # layer of current node's available dependencies
         # order the successors based on the order in the derive method; this allows the
         # class to define the best path through the dependency tree.
@@ -226,12 +242,12 @@ def dependencies3(di_graph, root, node_mgr, raise_cir_dep=False):
                 layer.add(dependency)
             # each time traverse_tree returns, remove node from visited path
             path.pop()
-            
+
         if node_mgr.operational(node, layer):
             # node will work at this level with the available dependencies
             active_nodes.add(node)
             ordering.append(node)
-            
+
             if node not in node_mgr.hdf_keys:
                 tree_path.append(list(path))
             return True  # layer below works
@@ -239,7 +255,7 @@ def dependencies3(di_graph, root, node_mgr, raise_cir_dep=False):
             # node will not work with available dependencies
             tree_path.append(list(path) + ['NOT OPERATIONAL',])
             return False
-        
+
     ordering = []
     path = deque()  # current branch path
     active_nodes = set()  # operational nodes visited for fast lookup
@@ -247,17 +263,17 @@ def dependencies3(di_graph, root, node_mgr, raise_cir_dep=False):
     path_start_kpv = []#set()
     tree_path = [] # For viewing the tree in which nodes are add to path
     traverse_tree(root)  # start recursion
-    return ordering
+    return ordering, tree_path
 
 
 def draw_graph(graph, name, horizontal=False):
     """
     Draws a graph to file with label and filename taken from name argument.
-    
+
     Note: Graphviz binaries cannot be easily installed on Windows (you must
     build it from source), therefore you shouldn't bother trying to
     draw_graph unless you've done so!
-    
+
     :param graph: Dependency graph to draw
     :type graph: nx.DiGraph
     :param name: Name of graph being drawn. Added to filename: graph_[name].ps
@@ -297,10 +313,10 @@ def any_predecessors_in_requested(node_name, requested, graph):
     Recursively move towards the start of the tree (those which depend upon
     this node) searching for a node predecessor that's within the list of
     requested nodes.
-    
+
     If the node itself has been requested, and none of its predecessors are
     requested, the result is "False".
-    
+
     :param node_name: Name of the node to recurse down the graph
     :type node_name: String / object
     :param requested: List of nodes requested
@@ -322,7 +338,7 @@ def graph_adjacencies(graph):
     '''
     Create a dictionary of each nodes adjacencies within the graph. Useful for
     JIT javascript presentation of the tree.
-    
+
     :param graph: Dependency tree graph
     :type graph: nx.Graph
     :returns: Restructured graph
@@ -401,14 +417,14 @@ def graph_nodes(node_mgr):
             # This node is required to build the tree
             root_edges.append(('root', node_req))
     gr_all.add_edges_from(root_edges) ##, color='red')
-    
+
     #TODO: Split this up into the following lists of nodes
     # * LFL used
     # * LFL unused
     # * Derived used
     # * Derived not operational
     # * Derived not used -- coz not referenced by a dependency kpv etc therefore not part of the spanning tree
-    
+
     # Note: It's hard to tell whether a missing dependency is a mistyped
     # reference to another derived parameter or a parameter not available on
     # this LFL
@@ -419,7 +435,7 @@ def graph_nodes(node_mgr):
     missing_derived_dep = list(derived_deps - available_nodes)
     # Missing dependencies which are requested.
     missing_requested = list(set(node_mgr.requested) - available_nodes)
-    
+
     if missing_derived_dep:
         logger.warning("Found %s dependencies which don't exist in LFL "
                        "or Node modules.", len(missing_derived_dep))
@@ -435,34 +451,36 @@ def graph_nodes(node_mgr):
     return gr_all
 
 
-def process_order(gr_all, node_mgr, raise_inoperable_requested=False, raise_cir_dep=False):
+def process_order(gr_all, node_mgr, raise_inoperable_requested=False,
+                  raise_cir_dep=False, path_tree_file=None):
     """
     :param gr_all:
     :type gr_all: nx.DiGraph
-    :param node_mgr: 
+    :param node_mgr:
     :type node_mgr: NodeManager
     :returns:
-    :rtype: 
+    :rtype:
     """
-    process_order = dependencies3(gr_all, 'root', node_mgr, raise_cir_dep=raise_cir_dep)
+    process_order, tree_path = dependencies3(gr_all, 'root', node_mgr, raise_cir_dep=raise_cir_dep)
     logger.debug("Processing order of %d nodes is: %s", len(process_order), process_order)
-    
+    if path_tree_file:
+        ordered_tree_to_file(tree_path, name=path_tree_file)
     for n, node in enumerate(process_order):
         gr_all.node[node]['label'] = '%d: %s' % (n, node)
         gr_all.node[node]['active'] = True
-        
+
     inactive_nodes = set(gr_all.nodes()) - set(process_order)
     logger.debug("Inactive nodes: %s", list(sorted(inactive_nodes)))
     gr_st = gr_all.copy()
     gr_st.remove_nodes_from(inactive_nodes)
-    
+
     for node in inactive_nodes:
         # add attributes to the node to reflect it's inactivity
         gr_all.node[node]['color'] = '#c0c0c0'  # silver
         gr_all.node[node]['active'] = False
         inactive_edges = gr_all.in_edges(node)
         gr_all.add_edges_from(inactive_edges, color='#c0c0c0')  # silver
-        
+
     inoperable_requested = list(set(node_mgr.requested) - set(process_order))
     if inoperable_requested:
         logger.warning("Found %s inoperable requested parameters.",
@@ -475,15 +493,15 @@ def process_order(gr_all, node_mgr, raise_inoperable_requested=False, raise_cir_
                 if tree:
                     items.append('------- INOPERABLE -------')
                     items.extend(tree)
-            logger.debug('\n'+'\n'.join(items))        
+            logger.debug('\n'+'\n'.join(items))
         if raise_inoperable_requested:
             raise InoperableDependencies(inoperable_requested)
-    
+
     required_missing = set(node_mgr.required) - set(process_order)
     if required_missing:
         raise RequiredNodesMissing(
             "Required nodes missing: %s" % ', '.join(required_missing))
-    
+
     return gr_all, gr_st, process_order[:-1] # exclude 'root'
 
 
@@ -498,14 +516,15 @@ def remove_floating_nodes(graph):
         if not graph.predecessors(node) and not graph.successors(node):
             graph.remove_node(node)
     return graph
-     
-     
+
+
 def dependency_order(node_mgr, draw=not_windows,
-                     raise_inoperable_requested=False, raise_cir_dep=False):
+                     raise_inoperable_requested=False, raise_cir_dep=False,
+                     path_tree_file=None):
     """
     Main method for retrieving processing order of nodes.
-    
-    :param node_mgr: 
+
+    :param node_mgr:
     :type node_mgr: NodeManager
     :param draw: Will draw the graph. Green nodes are available LFL params, Blue are operational derived, Black are not requested derived, Red are active top level requested params, Grey are inactive params. Edges are labelled with processing order.
     :type draw: boolean
@@ -515,8 +534,8 @@ def dependency_order(node_mgr, draw=not_windows,
     _graph = graph_nodes(node_mgr)
     gr_all, gr_st, order = process_order(_graph, node_mgr,
                                          raise_inoperable_requested=raise_inoperable_requested,
-                                         raise_cir_dep=raise_cir_dep)
-    
+                                         raise_cir_dep=raise_cir_dep, path_tree_file=path_tree_file)
+
     if draw:
         from json import dumps
         logger.info("JSON Graph Representation:\n%s", dumps(graph_adjacencies(gr_st), indent=2))

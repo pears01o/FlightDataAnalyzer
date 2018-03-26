57d393866b06dec351082b5cbd1625715a880909
Richard Hoy <richard.hoy@flightdataservices.com>
2018-03-26 09:53:03 +0100

    AE-1729: Add ability to print the ordered tree

--- analysis_engine/utils.py
+++ analysis_engine/utils.py
@@ -222,7 +222,7 @@ def derived_trimmer(hdf_path, node_names, dest):
             derived_nodes, {}, {})
         _graph = graph_nodes(node_mgr)
         for node_name in node_names:
-            deps = dependencies3(_graph, node_name, node_mgr)
+            deps, _ = dependencies3(_graph, node_name, node_mgr)
             params.extend(filter(lambda d: d in node_mgr.hdf_keys, deps))
     return strip_hdf(hdf_path, params, dest)
 

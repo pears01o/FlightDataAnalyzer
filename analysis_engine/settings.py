5bd2456cfdfd7b6e4d09e5524f6eff1e14a733e8
Dave Jesse <dave.jesse@flightdataservices.com>
2018-08-01 16:42:08 +0100

    TCAS SCAN TIME setting

--- analysis_engine/settings.py
+++ analysis_engine/settings.py
@@ -391,7 +391,8 @@ MU_POOR = 0.05 # dimensionless.
 '''
 
 # Pilot action threshold for TCAS responses
-TCAS_THRESHOLD = 0.15 # g
+TCAS_SCAN_TIME = 10.0 # seconds
+TCAS_THRESHOLD = 0.1 # g
 
 # Transition altitude - above use Altitude STD, below use Altitude AAL.
 # Note: This affects the generic KTIs and dependent nodes only. Some KPVs are 

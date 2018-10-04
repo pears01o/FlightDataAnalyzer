75899057a36e0f28ac9f29e25b7ffc952d7d2897
Dave Jesse <dave.jesse@flightdataservices.com>
2018-10-04 18:06:39 +0100

    TCAS state names used for RA cases.

--- analysis_engine/flight_phase.py
+++ analysis_engine/flight_phase.py
@@ -2282,17 +2282,41 @@ class TCASResolutionAdvisory(FlightPhaseNode):
             # We can be sloppy about error conditions because these have been taken 
             # care of in the TCAS Operational definition.
             if tcas_cc:
-                ra_slices = np.ma.clump_unmasked(np.ma.masked_less(tcas_cc.array.data[tcas_op.slice], 4))
-                dn_slices = np.ma.clump_unmasked(np.ma.masked_less(tcas_da.array.data[tcas_op.slice], 1))
-                up_slices = np.ma.clump_unmasked(np.ma.masked_less(tcas_ua.array.data[tcas_op.slice], 1))
+                ra_slices = runs_of_ones(tcas_cc.array[tcas_op.slice].any_of(
+                    'Up Advisory Corrective',
+                    'Down Advisory Corrective',
+                    'Preventive',
+                    ignore_missing=True,
+                ))
+    
+                dn_slices = runs_of_ones(tcas_da.array[tcas_op.slice].any_of(
+                    "Descent",
+                    "Don't Climb",
+                    "Don't Climb > 500",
+                    "Don't Climb > 1000",
+                    "Don't Climb > 2000",
+                    ignore_missing=True,
+                ))
+    
+                up_slices = runs_of_ones(tcas_ua.array[tcas_op.slice].any_of(
+                    "Climb",
+                    "Don't Descend",
+                    "Don't Descend > 500",
+                    "Don't Descend > 1000",
+                    "Don't Descend > 2000",            
+                    ignore_missing=True,
+                ))
                 
                 ra_slices = shift_slices(slices_or(ra_slices, dn_slices, up_slices), tcas_op.slice.start)
+
                 hz = tcas_cc.frequency
                 
             else:
                 # Operating with only a single TCAS RA signal, as recorded on some aircraft.
-                ra_slices =  np.ma.clump_unmasked(np.ma.masked_less(tcas_ra.array.data[tcas_op.slice], 1))
-                
+                ra_slices = runs_of_ones(tcas_ra.array[tcas_op.slice].any_of(
+                    'RA',
+                    ignore_missing=True,
+                ))                
                 ra_slices = shift_slices(ra_slices, tcas_op.slice.start)
                 hz = tcas_ra.frequency
             

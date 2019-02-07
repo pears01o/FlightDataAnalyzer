6071d7e6dd0b9d3b42ca567769bf4dbca3eab377
Glen Kirkup <glen.kirkup@flightdataservices.com>
2019-02-07 14:04:13 +0000

    AE-1914: fix typo in comment

--- tests/derived_parameters_test.py
+++ tests/derived_parameters_test.py
@@ -1281,7 +1281,7 @@ class TestAltitudeAGL(unittest.TestCase):
         gog = M(name='Gear On Ground', array=np.ma.array([1]*5+[0]*90+[1]*5), values_mapping={0:'Air', 1:'Ground'})
         alt_aal = AltitudeAGL()
         alt_aal.derive(alt_rad, None, alt_baro, gog)
-        # if the setting.ALTITUDE_AGL_TRANS_ALT is change the expected result will be affected.
+        # if the setting.ALTITUDE_AGL_TRANS_ALT is changed the expected result will be affected.
         self.assertLess(abs(np.max(alt_aal.array)-8000), 350)
 
     def test_negative(self):
